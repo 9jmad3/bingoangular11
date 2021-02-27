f3d72eab21f8ebce315bdbb8149cb32ad9e35ec9
9jmad3 <65808538+9jmad3@users.noreply.github.com>
2021-02-27 22:01:50 +0100

    final

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const path = require('path');
+// const express = require('express');
+// const path = require('path');
 
-const app = express();
+// const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/bingo-dos'));
-});
+// app.use(express.static(__dirname+'/dist/ng-blog'));
+// app.get('/',function(req,res){
+//     res.sendFile(path.join(__dirname+'/dist/ng-blog/bingo-dos'));
+// });
 
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
 
 // ORIGINALES DE PACKAGE.JSON
 // {
